4246181c804216f717ef87b99f2ffe130a5da193
Parteek Saini <sainiparteek18@gmail.com>
2022-05-31 01:21:33 -0700

    Done Code Organisation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "./App.css";
-import Header from "./Header";
-import Home from "./Home";
-
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Checkout from "./components/Checkout/Checkout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="app">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Checkout />
+              </>
+            }
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
