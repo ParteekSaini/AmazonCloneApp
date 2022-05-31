cdfd326530083042d4e1506a91b3f088b299bf33
Parteek Saini <sainiparteek18@gmail.com>
2022-05-30 18:36:55 -0700

    header design

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA-VQ9qk1UrfsM5zg6ZPUTx3MBH2Ef5qKU",
+  authDomain: "clone-ee6fa.firebaseapp.com",
+  projectId: "clone-ee6fa",
+  storageBucket: "clone-ee6fa.appspot.com",
+  messagingSenderId: "550188167370",
+  appId: "1:550188167370:web:f5e98acd6183b2625b4433",
+  measurementId: "G-DE3FQFL3MV",
+};
